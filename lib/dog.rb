97a495ae3c0117ae084c41eb4967c3eba4d96ae2
Dakota Clark <bannaray95@gmail.com>
2018-11-12 01:43:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,3 @@
 class Dog
   
-  def initialize(name)
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def initialize(breed)
-    @breed = breed
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-end
+  def initialize(dogs_name, 
